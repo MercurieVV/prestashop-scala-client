dbb66e1d46560dd4cf96b560f9613d67c841208e
MercurieVV <mercurievv@gmail.com>
2018-11-16 22:01:15 +0200

    Add estimated delivery date

--- src/main/scala/co/orderly/prestasac/representations/Order.scala
+++ src/main/scala/co/orderly/prestasac/representations/Order.scala
@@ -99,6 +99,10 @@ case class OrderElement(
 
                          @xmlElement(nillable = true)
                          @xmlJavaTypeAdapter(classOf[DateSpaceTimeAdapter])
+                         var estimatedDeliveryDate: JDate,
+
+                         @xmlElement(nillable = true)
+                         @xmlJavaTypeAdapter(classOf[DateSpaceTimeAdapter])
                          var deliveryDate: JDate,
                          var valid: JInteger,
 
@@ -159,7 +163,7 @@ case class OrderElement(
                          @xmlElement(required = true)
                          var associations: Associations,
                        ) extends PrestaShopTimestampedIdentity with Representation {
-  private def this() = this(null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null)
+  private def this() = this(null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null)
 }
 
 /**
